93a33fd743a37fa807c74aef63a29a69e1c7abb8
IvanTheEwok <ivan52x53@gmail.com>
2018-05-22 21:05:09 +0200

    Added model for clues and items

--- app/models.py
+++ app/models.py
@@ -10,3 +10,24 @@ class User(db.Model):
 
     def __repr__(self):
         return "<User {}>".format(self.username)
+
+class Clue(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tier = db.Column(db.String(6))
+    value = db.Column(db.Integer)
+    stages = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def __repr__(self):
+        return "<Clue {id}, {tier}>".format(id=self.id, tier=self.tier)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item = db.Column(db.String(50))
+    amount = db.Column(db.Integer)
+    value_each = db.Column(db.Integer)
+    clue_id = db.Column(db.Integer, db.ForeignKey("clue.id"))
+
+    def __repr__(self):
+        return "<Item {item}, amount {amount}>".format(item=self.item, amount=self.amount)
