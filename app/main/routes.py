8c1d902831a5163e2eb620ce8e639a4b3f3851a2
IvanTheEwok <ivan52x53@gmail.com>
2018-05-25 16:58:14 +0200

    Added follow functions and admin functionality to edit_profile and user profile

--- app/main/routes.py
+++ app/main/routes.py
@@ -4,13 +4,15 @@ from app import db
 from flask_login import current_user, login_required
 from datetime import datetime
 from app.models import User, Clue
-from app.main.forms import EditProfileForm
+from app.main.forms import EditProfileForm, EditProfileFormAdmin
 
 @bp.route("/")
 @bp.route("/index")
 def index():
+    site_admins = current_app.config["SITE_ADMINS"]
     return render_template(
-        "main/index.html"
+        "main/index.html",
+        site_admins=site_admins
     )
 
 @bp.before_request
@@ -21,6 +23,7 @@ def before_request():
     
 @bp.route("/user/<username>")
 def user(username):
+    site_admins = current_app.config["SITE_ADMINS"]
     user = User.query.filter_by(username=username.lower()).first_or_404()
     page = request.args.get("page", 1, type=int)
     clues = user.clues.order_by(Clue.timestamp.desc()).paginate(
@@ -40,6 +43,7 @@ def user(username):
     ) if clues.has_prev else None
     return render_template(
         "main/user.html",
+        site_admins=site_admins,
         user=user,
         clues=clues.items,
         next_url=next_url,
@@ -49,19 +53,66 @@ def user(username):
 @bp.route("/edit_profile", methods=["GET", "POST"])
 @login_required
 def edit_profile():
-    form = EditProfileForm(current_user.username)
+    site_admins = current_app.config["SITE_ADMINS"]
+    if current_user.id in site_admins:
+        form = EditProfileFormAdmin()
+        if form.validate_on_submit():
+            user_id = form.user_id.data
+            user = User.query.filter_by(id=user_id).first()
 
-    if form.validate_on_submit():
-        current_user.username = form.username.data.lower()
-        current_user.about_me = form.about_me.data
-        db.session.commit()
-        flash("Your changes have been saved.")
-        return redirect(url_for("main.edit_profile"))
-    elif request.method == "GET":
-        form.username.data = current_user.username.lower()
-        form.about_me.data = current_user.about_me
+            if form.user_email.data:
+                user.email = form.user_email.data
+            if form.username.data:
+                user.username = form.username.data.lower()
+            if form.about_me.data:
+                user.about_me = form.about_me.data
+            db.session.commit()
+            flash("Your changes have been saved.")
+            return redirect(url_for("main.edit_profile"))
+    else:
+        form = EditProfileForm(current_user.username)
+        if form.validate_on_submit():
+            current_user.username = form.username.data.lower()
+            current_user.about_me = form.about_me.data
+            db.session.commit()
+            flash("Your changes have been saved.")
+            return redirect(url_for("main.edit_profile"))
+        elif request.method == "GET":
+            form.username.data = current_user.username.lower()
+            form.about_me.data = current_user.about_me
     return render_template(
         "main/edit_profile.html",
         title="Edit profile",
         form=form
     )
+
+@bp.route("/follow/<username>")
+@login_required
+def follow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash ("User {} not found.").format(username)
+        return redirect(url_for("main.index"))
+    if user == current_user:
+        flash("You can't follow yourself!")
+        return redirect(url_for("main.user", username=username))
+    current_user.follow(user)
+    db.session.commit()
+    flash("You are now following {}!".format(username))
+    return redirect(url_for("main.user", username=username))
+
+@bp.route("/unfollow/<username>")
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash("User {} not found.".format(username))
+        return redirect(url_for("main.index"))
+    if user == current_user:
+        flash("You can't unfollow yourself")
+        return redirect(rul_for("main.user", username=username))
+    
+    current_user.unfollow(user)
+    db.session.commit()
+    flash("You are not following {} anymore.".format(username))
+    return redirect(url_for("main.user", username=username))
