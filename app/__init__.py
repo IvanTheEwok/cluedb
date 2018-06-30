2049ec06204d90e377a0a0f028b39c23b6d60f6c
IvanTheEwok <ivan52x53@gmail.com>
2018-06-30 23:09:44 +0200

    Added mailing of errors to admins

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_moment import Moment
 from config import Config
+import logging
+from logging.handlers import SMTPHandler
 
 #Create variable for extensions
 db = SQLAlchemy()
@@ -39,6 +41,23 @@ def create_app(config_class=Config):
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
+    if not app.debug:
+        if app.config["MAIL_SERVER"]:
+            auth = None
+            if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+                auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+            secure = None
+            if app.config["MAIL_USE_TLS"]:
+                secure = ()
+                mail_handler = SMTPHandler(
+                    mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
+                    fromaddr="no-replu@" + app.config["MAIL_SERVER"],
+                    toaddrs=app.config["ADMINS"],
+                    subject="Cluedb failure",
+                    credentials=auth, secure=secure
+                )
+                mail_handler.setLevel(logging.ERROR)
+                app.logger.addHandler(mail_handler)
     return app
 
 from app import models
