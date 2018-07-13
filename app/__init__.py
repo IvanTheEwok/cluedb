43412db17888486462fa16f7d0da13aa233c64dd
IvanTheEwok <ivan52x53@gmail.com>
2018-07-13 18:09:41 +0200

    Added error logging

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,8 @@ from flask_login import LoginManager
 from flask_moment import Moment
 from config import Config
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 #Create variable for extensions
 db = SQLAlchemy()
@@ -58,6 +59,19 @@ def create_app(config_class=Config):
             )
             mail_handler.setLevel(logging.ERROR)
             app.logger.addHandler(mail_handler)
+
+        if not os.path.exists("logs"):
+            os.mkdir("logs")
+        file_handler = RotatingFileHandler("logs/cluedb.log", maxBytes=10240,
+                                           backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+        ))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info("Cluedb startup")
     return app
 
 from app import models
